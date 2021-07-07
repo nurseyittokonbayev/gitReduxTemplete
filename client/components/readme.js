bc064ab77370cc25b47e9a577aacd429a3425782
Nurseyit <user@Nurseyit-Tokonbayev.local>
2021-07-07 23:17:28 +0300

    add main css

--- client/components/readme.js
+++ client/components/readme.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Markdown from 'markdown-to-jsx'
 import { useSelector } from 'react-redux'
 
+
 const Readme = () => {
   const readme = useSelector((s) => s.data.readme)
   return (
